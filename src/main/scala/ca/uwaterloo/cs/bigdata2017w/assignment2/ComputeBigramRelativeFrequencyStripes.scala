6b0bd98b2f6afb8f7856a03a87932843f71978fe
y33xiang <y33xiang@student.cs.uwaterloo.ca>
2017-01-25 00:55:20 -0500

    add files

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyStripes.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyStripes.scala
@@ -25,52 +25,54 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
-    mainOptions = Seq(input, output, reducers)
-    val input = opt[String](descr = "input path", required = true)
-    val output = opt[String](descr = "output path", required = true)
-    val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
-    verify()
+mainOptions = Seq(input, output, reducers)
+val input = opt[String](descr = "input path", required = true)
+val output = opt[String](descr = "output path", required = true)
+val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+verify()
 }
 
-    object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
-    val log = Logger.getLogger(getClass().getName())
+object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
+val log = Logger.getLogger(getClass().getName())
 
-    def main(argv: Array[String]) {
-    val args = new Conf(argv)
+def main(argv: Array[String]) {
+val args = new Conf(argv)
 
-    log.info("Input: " + args.input())
-    log.info("Output: " + args.output())
-    log.info("Number of reducers: " + args.reducers())
+log.info("Input: " + args.input())
+log.info("Output: " + args.output())
+log.info("Number of reducers: " + args.reducers())
 
-    val conf = new SparkConf().setAppName("Bigram Count")
-    val sc = new SparkContext(conf)
+val conf = new SparkConf().setAppName("Bigram Count")
+val sc = new SparkContext(conf)
 
-    val outputDir = new Path(args.output())
-    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+val outputDir = new Path(args.output())
+FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-    val textFile = sc.textFile(args.input())
+val textFile = sc.textFile(args.input())
 
-    val countWord = textFile
-    .flatMap(line => tokenize(line))
-    .map(word => (word, 1))
-    .reduceByKey(_ + _)
+val countWord = textFile
+.flatMap(line => tokenize(line))
+.map(word => (word, 1))
+.reduceByKey(_ + _)
 
 
-    val countPair = textFile
-    .flatMap(line => {
-    val tokens = tokenize(line)
-    if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
-    })
-    .map(bigram => (bigram, 1))
-    .reduceByKey(_ + _)
-    .map(a => (a._1.split(" ")(0),(a._1.split(" ")(1),a._2)))
-    .join(countWord)
-    .map(a => (a._1,(a._2._1._1,((a._2._1._2).toDouble/(a._2._2).toDouble))))
-    .groupByKey()
+val countPair = textFile
+.flatMap(line => {
+val tokens = tokenize(line)
+if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
+})
+.map(bigram => (bigram, 1))
+.reduceByKey(_ + _)
+.map(a => (a._1.split(" ")(0),(a._1.split(" ")(1),a._2)))
+.join(countWord)
+.map(a => (a._1,(a._2._1._1,(((a._2._1._2).toDouble/(a._2._2).toDouble),a._2._1._2))))
+.groupByKey()
 
-    countPair.saveAsTextFile(args.output())
-//	.map{ something => a._1={a._2._1 = a._2._2} }.saveAsTextFile(args.output())
-    }
+
+
+countPair.saveAsTextFile(args.output())
+
+}
 }
 
 

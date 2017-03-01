b1b3dd403a3710ffe503483843c396a4bca1a158
y33xiang <y33xiang@student.cs.uwaterloo.ca>
2017-03-01 16:18:18 -0500

    add files

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q1.scala
@@ -1,4 +1,5 @@
-pin: reference implementations of "big data" algorithms
+/**
+  * Bespin: reference implementations of "big data" algorithms
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
